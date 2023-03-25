90af338a37e1c6a2789310b211e406816a4e844b
Md. Soliman Ali <65532591+Md-Soliman-Ali@users.noreply.github.com>
2023-03-25 21:14:35 +0600

    Delete src/test directory

--- src/test/java/RunCucumberTest.java
+++
@@ -1,9 +0,0 @@
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber-reports/cucumber.html", "json:target/cucumber-reports/cucumber.json"})
-
-public class RunCucumberTest {
-}
